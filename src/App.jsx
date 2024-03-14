bfeabefe1d6eec1a6637967b9c932fb9e913ff90
Christy Llewellyn <christy.elizabeth@hotmail.co.uk>
2024-03-14 20:33:19 +0000

    Created router to dynamically search for wikipedia terms

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'; // Import necessary components from react-router-dom
+import Description from '/Users/christy/Desktop/revision-app/src/components/Description/Description.jsx';
 
 function App() {
-
-  return <h1>Hello Team!</h1>
+  return (
+    <div>
+      <Router> {/* Wrap your routes with Router component */}
+        <div>
+          <Route path="/" exact render={() => <Description searchTerm="Home" />} />
+          <Route path="/about" render={() => <Description searchTerm="About" />} />
+          <Route path="/contact" render={() => <Description searchTerm="Contact" />} />
+          {/* Add more routes as needed */}
+        </div>
+      </Router> {/* Close the Router component */}
+      <div>
+        <h1>Hello Team!</h1>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
