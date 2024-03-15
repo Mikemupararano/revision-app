f58eb5953ede9711ed7a92f6f52ffc0d216c2bd9
Christy Llewellyn <christy.elizabeth@hotmail.co.uk>
2024-03-15 19:10:17 +0000

    Fixed router bug initial code is working and showing description

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'; // Import necessary components from react-router-dom
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'; // Import necessary components from react-router-dom
 import Description from '/Users/christy/Desktop/revision-app/src/components/Description/Description.jsx';
 
 function App() {
   return (
     <div>
-      <Router> {/* Wrap your routes with Router component */}
-        <div>
-          <Route path="/html" exact render={() => <Description searchTerm="HTML" />} />
-          <Route path="/css" render={() => <Description searchTerm="CSS" />} />
-          <Route path="/javascript" render={() => <Description searchTerm="JavaScript" />} />
-          {/* Add more routes as needed */}
-        </div>
-      </Router> {/* Close the Router component */}
-      <div>
-        <h1>Hello Team!</h1>
-      </div>
+      <h1>Hello Team!</h1>
+      <Router>
+        <Routes> {/* Use Routes component to define routes */}
+          {/* Define routes using Route component */}
+          <Route path="/html" element={<Description searchTerm="HTML" />} />
+          <Route path="/css" element={<Description searchTerm="CSS" />} />
+          {/* Add more routes for other pages */}
+        </Routes>
+      </Router>
     </div>
   );
 }
