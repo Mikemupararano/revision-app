31348e5e48ebf1e4fa667595c73e7a8dc1476fe4
designs-by-kate <kate_diet@hotmail.com>
2024-03-15 20:27:13 +0000

    edited colors based on provided palatte

--- src/components/Quiz/Question.jsx
+++ src/components/Quiz/Question.jsx
@@ -6,10 +6,12 @@ const Question = ({ question, currentQuestionIndex, handleAnswerClick, userAnswe
   return (
     <div className="question ml-sm-5 pl-sm-5 pt-2">
       <div className="py-2 h5 ">
-      <h2>{question.language.toUpperCase()} Question</h2>
+      <h2>{question.language.toUpperCase()} Question {currentQuestionIndex} / 10</h2>
       </div>
       <div className="ml-md-3 ml-sm-3 pl-md-5 pt-sm-0 pt-3" id="options">
+        <div className="questionText">
         <h1>{question.questionText}</h1>
+        </div>
         {question.answerOptions.map((option, index) => (
           <label key={index} className="options" style={{ color: userAnswers[currentQuestionIndex] === option.answerText ? (option.isCorrect ? '#218575' : 'red') : 'inherit' }}>
             {option.answerText}
@@ -26,17 +28,17 @@ const Question = ({ question, currentQuestionIndex, handleAnswerClick, userAnswe
       </div>
       <div className="buttonQ pt-3">
         <div id="prev">
-          <button className="btn-small text-black align-items-left" onClick={handlePreviousButtonClick} disabled={currentQuestionIndex === 0}>
+          <button className="btn-small"  style={{ color: '#218575' }} onClick={handlePreviousButtonClick} disabled={currentQuestionIndex === 0}>
             Previous
           </button>
         </div>
-        <div className="ml-auto mr-sm-5 align-items-right ">
+        <div className="ml-auto mr-sm-5">
           {isLastQuestion ? (
-            <button className="btn-small text-black" onClick={handleNextButtonClick}>
+            <button className="btn-small" style={{ color: '#218575' }} onClick={handleNextButtonClick}>
               Finish
             </button>
           ) : (
-            <button className="btn-small text-danger " onClick={handleNextButtonClick}>
+            <button className="btn-small" style={{ color: '#ff680a' }} onClick={handleNextButtonClick}>
               Next
             </button>
           )}
