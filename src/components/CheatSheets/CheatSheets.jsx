18650f14f4926550e11c4b009f722a598cebc50d
Quarshie <asi37375@hotmail.com>
2024-03-15 18:45:44 +0000

    css style added to component

--- src/components/CheatSheets/CheatSheets.jsx
+++ src/components/CheatSheets/CheatSheets.jsx
@@ -1,34 +1,22 @@
 import { useState } from 'react';
 import { Document, Page } from 'react-pdf';
-import pdf from './Javascript PDF'
 
 function CheatSheets() {
     const [numPages, setNumPages] = useState();
     const [pageNumber, setPageNumber] = useState(1);
 
-    function onDocumentLoadSuccess({ numPages }):
+    function onDocumentLoadSuccess({ numPages }) {
         setNumPages(numPages);
+    }
 
+    return (
+        <div>
+            <Document file="pdf" onLoadSuccess={onDocumentLoadSuccess}>
+                <Page pageNumber={pageNumber} />
+            </Document>
+            <p>
+                Page {pageNumber} of {numPages}
+            </p>
+        </div>
+    );
 }
-
-return (
-    <div>
-        <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
-            {Array.apply(null, Array(numPages))
-                .map((x, i) => i + 1)
-                .map(page)=>) {
-                return(
-            <Page
-                pageNumber={page}
-                renderTextlayer={false}
-                renderAnnotationLayer={false}
-            />;
-            );
-            }
-        </Document>
-        <p>
-            Page {pageNumber} of {numPages}
-        </p>
-    </div>
-);
-export defualt CheatSheets;
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as bootstrap from 'bootstrap';
 import { pdfjs } from 'react-pdf';
-import CheatSheets from './CheatSheets'; // Import the CheatSheets component
+import CheatSheets from "./assets/CheatSheets"; // Import the CheatSheets component
 
 // Set up PDF worker source
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
@@ -12,10 +12,12 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   import.meta.url,
 ).toString();
 
-// Render your application
+// Render application
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <CheatSheets /> {/* Render CheatSheets component here */}
   </React.StrictMode>
 );
+
+<CheatSheets/>
