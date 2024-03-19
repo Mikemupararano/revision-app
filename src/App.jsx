f41b568a4e9df3eb99f3844fa27317f5e88dbdb0
Andreea-Lita <litaandreeamalaiasi@gmail.com>
2024-03-19 15:25:39 +0000

    fixing the code functionality so it displays code Snippet and imports all elements in App.jsx, fixed main.jsx too, added github folder workflows as a start for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,44 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+import HomePage from './components/Home/Home.jsx';
+import Footer from './components/Footer/Footer.jsx';
+//import ModulePage from './ModulePage/ModulePage';
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary.jsx';
+import HTML from './components/HTML-course/HTML.jsx'
+import CheatSheets from './components/CheatSheets/CheatSheets.jsx';
+import JavaScript from './components/JavaScript/JavaScript.jsx';
+import CodeSection from './components/CodeSection/CodeSection.jsx';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CssCourse from './components/CssCourse/CssCourse.jsx';
+import Navbar from './components/Navbar/Navbar.jsx';
+import Quiz from './components/Quiz/Quiz.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        < Navbar/>
+        <ErrorBoundary>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          {/* <Route path="/modules" element={<ModulePage />} /> */}
+          <Route path="/html" element={<HTML />} />
+          <Route path="/cssCourse" element={<CssCourse />} />
+          <Route path="/javaScript" element={<JavaScript />} />
+          <Route path="/cheatSheets" element={<CheatSheets />} />
+          <Route path="/codeSection" element={<CodeSection />} />
+          <Route path="/quiz" element={<Quiz />} />
+          
+
+          {/* Add other routes as necessary */}
+        </Routes>
+        </ErrorBoundary>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
+import ReactDOM from 'react-dom/client';
 
-createRoot(document.getElementById('root')).render(
-  <ErrorBoundary>
+import App from './App';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
     <App />
-  </ErrorBoundary>
+  </React.StrictMode>
 );
+
+
+// import { createRoot } from 'react-dom/client';
+// import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
+// createRoot(document.getElementById('root')).render(
+//   <ErrorBoundary>
+//     <App />
+//   </ErrorBoundary>
+// );
